7bdd632a15de1c93694be38018761e05f897e0cb
Nafis Ahamed <nafisahamed14@gmail.com>
2024-05-07 20:02:07 +0600

    pagination

--- pagination.js
+++ pagination.js
@@ -1,24 +1,46 @@
+ 
+//   const [itemsPerPages,setItemsPerPages] = useState(50)
+//   const [currentPage,setCurrentPage] = useState(0)
+//   const {count} = useLoaderData()
 
+//   const numberOfPages = Math.ceil(count / itemsPerPages)
 
-// const [itemsPerPages,setItemsPerPages] = useState(50)
-// const {count} = useLoaderData()
-
-// const numberOfPages = Math.ceil(count / itemsPerPages)
-
-// const pages = [...Array(numberOfPages).keys()]
+//   const pages = [...Array(numberOfPages).keys()]
 
 
 // const handleItemsperpage = e =>{
-   
+       
 //     const val = parseInt(e.target.value)
 //     console.log(val);
 //     setItemsPerPages(val)
 //     console.log(itemsPerPages);
+//     setCurrentPage(0)
     
-//   <div className='pagination'> 
-// {
-//    pages.map(page => <button className='btn btn-primary'>{page}</button> )
 // }
+
+// const handlePrev = e =>{
+//       if(currentPage > 0){
+//          setCurrentPage(currentPage - 1)
+//       }
+// }
+
+// const handleNext = ()=>{
+//      if(currentPage <pages.length - 1){
+//           setCurrentPage(currentPage + 1)
+//      }
+// }
+
+
+
+
+// <div className='pagination'> 
+// <p>Current Page: {currentPage} </p>
+// <button className='btn' onClick={handlePrev} >Prev</button>
+// {
+   
+//    pages.map(page => <button onClick={()=>setCurrentPage(page)} className={currentPage === page && 'selected' }>{page}</button> )
+// }
+// <button onClick={handleNext} className='btn'>Next</button>
 // <select value={itemsPerPages} name="" id="" onChange={handleItemsperpage}>
 //    <option value="5">5</option>
 //    <option value="10">10</option>
