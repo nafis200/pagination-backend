b482a9293f0e749aa0f439fc9077e303b4ff54fb
Nafis Ahamed <nafisahamed14@gmail.com>
2024-05-07 18:27:33 +0600

    pagination

---
+++ pagination.js
@@ -0,0 +1,28 @@
+
+
+// const [itemsPerPages,setItemsPerPages] = useState(50)
+// const {count} = useLoaderData()
+
+// const numberOfPages = Math.ceil(count / itemsPerPages)
+
+// const pages = [...Array(numberOfPages).keys()]
+
+
+// const handleItemsperpage = e =>{
+   
+//     const val = parseInt(e.target.value)
+//     console.log(val);
+//     setItemsPerPages(val)
+//     console.log(itemsPerPages);
+    
+//   <div className='pagination'> 
+// {
+//    pages.map(page => <button className='btn btn-primary'>{page}</button> )
+// }
+// <select value={itemsPerPages} name="" id="" onChange={handleItemsperpage}>
+//    <option value="5">5</option>
+//    <option value="10">10</option>
+//    <option value="20">20</option>
+//    <option value="50">50</option>
+// </select>
+// </div>
